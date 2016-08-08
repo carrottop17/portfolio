5fe271957062a47bb540f6b1c536ad3d7522d596
Daniel Barranco <danielbarranco@gmail.com>
2016-08-07 22:33:22 -0400

    updates to logo and links

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,11 @@
 
-$('body').scrollspy({ target: '.navbar' })
+$('body').scrollspy({ target: '.navbar' });
+
+// var offset = 60;
+
+// $('.navbar li a').click(function(event) {
+//     event.preventDefault();
+//     $($(this).attr('href'))[0].scrollIntoView();
+//     scrollBy(0, -offset);
+// });
+
