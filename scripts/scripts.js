0c6bc01cd523a58db18aa81e5127c29ef291e8a5
Daniel Barranco <danielbarranco@gmail.com>
2016-08-29 21:55:12 -0400

    added mix it up and slide animation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,25 @@
 
 $('body').scrollspy({ target: '.navbar' });
 
-// var offset = 60;
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
 
-// $('.navbar li a').click(function(event) {
-//     event.preventDefault();
-//     $($(this).attr('href'))[0].scrollIntoView();
-//     scrollBy(0, -offset);
-// });
+$(function(){
 
+	// Instantiate MixItUp:
+
+	$('#Container').mixItUp();
+
+});
